e867bd59e9285c48b96a78f706c19c59ac808e83
KoolKoder1000 <69100288+KoolKoder1000@users.noreply.github.com>
2026-02-06 02:16:54 +0200

    Update epsilon_main.py

--- epsilon_main.py
+++ epsilon_main.py
@@ -7,7 +7,7 @@ from streamlit_autorefresh import st_autorefresh
 st.set_page_config(page_title="אפסילון", page_icon="ε", layout="wide")
 
 # --- 2. Auto-Refresh ---
-st_autorefresh(interval=3000, key="epsilon_sidebar_final")
+st_autorefresh(interval=5000, key="epsilon_sidebar_final")
 
 # --- 3. Supabase ---
 url = st.secrets["SUPABASE_URL"]
@@ -157,7 +157,7 @@ if tasks:
 
 # --- 7. Sidebar (Should now be on the left) ---
 with st.sidebar:
-    st.title("ניהול")
+    st.title("")
     with st.form("new_task"):
         subj = st.selectbox("קורס", ["חומרי תעופה", "מדר ח'", "מוצקים", "פיזיקה 2", "חדוא 2", "שרטוט הנדסי"])
         desc = st.text_input("תיאור")
